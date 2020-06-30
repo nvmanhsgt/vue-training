cbd6e17ef56046768a9481d87e01404440b46224
nvmanhsgt <nvmanh.sgt@gmail.com>
2020-07-01 00:11:24 +0700

    #1 Add bootstrap-vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import routes      from './router'
+import routes from './router'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+
+// import css
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+// Install VueRouter
 Vue.use(VueRouter)
 const router = new VueRouter({mode: 'history', routes});
 
